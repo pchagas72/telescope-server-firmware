859a4655f31d189cbdd65050e347558461ccfc1d
Pedro Chagas Silva <pedro.pcsilva@ufpe.br>
2025-11-17 12:36:57 -0300

    Added mutex to output LED

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ This firmware is used on a side project of mine.
 
 ## TODO
 
-- [ ] Add mutex to output LEDs
+- [X] Add mutex to output LEDs
 - [ ] Add base name when receiving messages
 - [ ] Add logging
 - [ ] Add motor controls
--- main/telescope_server.c
+++ main/telescope_server.c
@@ -20,12 +20,21 @@
 #include "esp_wifi.h"
 
 #include "mqtt_client.h"
+#include <pthread.h>
 
 #define BLINK_LED 2
 #define MQTT_TOPIC "home/testing"
 #define MQTT_QOS 1
 
-static void blink_led(int n, int delay);
+typedef struct server_state{
+    bool connected_to_internet;
+    bool connected_to_mqtt;
+} Server_State;
+
+Server_State state;
+pthread_mutex_t led_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void blink_led(int n, int delay, pthread_mutex_t *led_mutex);
 static void mqtt_event_handler(void *handler_args,
         esp_event_base_t base,
         int32_t event,
@@ -35,6 +44,7 @@ void app_main(void)
 {
     char *taskName = pcTaskGetName(NULL);
     ESP_LOGI(taskName, "Task starting up\n");
+
 
     // Start internet connection
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -46,7 +56,8 @@ void app_main(void)
     wifi_ap_record_t ap_info;
     if (esp_wifi_sta_get_ap_info(&ap_info) == ESP_OK){
         ESP_LOGI("WiFi", "Connected to %s\n", ap_info.ssid);
-        blink_led(3, 100);
+        state.connected_to_internet = true;
+        blink_led(3, 100, &led_mutex);
     }
 
     esp_mqtt_client_config_t mqtt_cfg = {
@@ -57,16 +68,20 @@ void app_main(void)
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
     esp_mqtt_client_start(client);
 
-    blink_led(3, 500);
+    blink_led(3, 500, &led_mutex);
     while (1)
     {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        if (!state.connected_to_mqtt || !state.connected_to_internet) {
+            blink_led(5, 50, &led_mutex);
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
 
 // TODO: ADD MUTEX TO CONTROL THIS FUNCTION
-void blink_led(int n, int delay)
+void blink_led(int n, int delay, pthread_mutex_t *led_mutex)
 {
+    pthread_mutex_lock(led_mutex);
     gpio_reset_pin(BLINK_LED);
     gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
 
@@ -78,6 +93,7 @@ void blink_led(int n, int delay)
     }
 
     vTaskDelay(500 / portTICK_PERIOD_MS);
+    pthread_mutex_unlock(led_mutex);
 }
 
 // This is a default function that will always be called
@@ -89,7 +105,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI("MQTT", "Connected to Broker!");
-        blink_led(3, 100);
+        state.connected_to_mqtt = true;
+        blink_led(3, 100, &led_mutex);
 
         esp_mqtt_client_subscribe(client, MQTT_TOPIC, MQTT_QOS);
         ESP_LOGI("MQTT", "Connected to %s", MQTT_TOPIC);
@@ -103,17 +120,18 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 
     case MQTT_EVENT_DATA:
         if (strncmp(event->data, "sv.ping", event->data_len) == 0) {
-            blink_led(1, 100);
+            blink_led(1, 100, &led_mutex);
             ESP_LOGI("MQTT", "Received ping from base\n"); // TODO: ADD BASE_ID TO MSG
             char response_payload[64];
             snprintf(response_payload, sizeof(response_payload), "BRAVO: received ping packet.");
             esp_mqtt_client_publish(client, "home/testing", response_payload, 0, 1, 0);
-            blink_led(1, 100);
+            blink_led(1, 100, &led_mutex);
         }
         break;
 
     case MQTT_EVENT_ERROR:
         ESP_LOGI("MQTT", "Error occurred");
+        state.connected_to_mqtt = false;
         break;
         
     default:
